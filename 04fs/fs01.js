4e691a0c4f1aca9086a173169be3b8bb47f8f99e
zhanghui.chen <172360937@qq.com>
2018-06-07 18:59:32 +0800

    fs模块

---
+++ 04fs/fs01.js
@@ -0,0 +1,14 @@
+
+
+/*
+    fs.stat  检测是文件还是目录
+    fs.mkdir 创建目录
+    fs.writeFile 创建写入文件
+    fs.appendFile 追加文件
+    fs.readFile 读取文件
+    fs.rename 重命名
+    fs.rmdir 删除目录
+    fs.unlink 删除文件
+*/
+
+
