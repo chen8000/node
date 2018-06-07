3591c079d6d91bed17cf2a54c138311682eb0db8
zhanghui.chen <172360937@qq.com>
2018-06-07 19:10:27 +0800

    fs.stat

--- 04fs/fs01.js
+++ 04fs/fs01.js
@@ -11,4 +11,26 @@
     fs.unlink 删除文件
 */
 
+//导入fs模块
+var fs = require('fs');
+
+//使用fs.stat方法检测是文件还是目录 
+//第一个参数为要检测的文件或目录名，第二个参数是一个函数，接收两个参数
+// error 也不是文件也不是目录，错误信息
+// stats 检测到的文件或目录对象
+fs.stat('html',(error,stats)=>{
+    if(error){
+        console.log(error);
+        return;
+    }
+
+    
+    //返回true  /  false
+
+    // isFile()  是否是文件
+    console.log('文件' + stats.isFile());
+
+    //isDirectory() 是否是目录
+    console.log('目录'+ stats.isDirectory());
+})
 
