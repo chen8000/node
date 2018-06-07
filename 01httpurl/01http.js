95902295d84dc4e3d4770fb7d62128eb19b6fca4
zhanghui.chen <172360937@qq.com>
2018-06-07 13:01:25 +0800

    http  url  模块

--- 01httpurl/01http.js
+++ 01httpurl/01http.js
@@ -1,13 +1,30 @@
+
+//引入http 模块
 var http = require('http');
 
+
+/*
+    createServer后面的匿名函数的参数  
+    request 获取url信息
+    response 浏览器返回响应信息
+*/
 http.createServer(function (request,response) {
 
+    //设置响应头，
+    //200 : ok  
+    //文件类型 : html
+    //字符编码 : utf-8
     response.writeHead(200,{'ContentType':'text/html;charset=utf-8'});
 
-    // if(!request.url !== '')
-    console.log('访问')
+    // if(request.url !== 'favicon.ico'){
+        console.log('访问');
+    // }
+    
+    //往页面输出内容
+    response.write('zhanghui.chen');
 
-    response.end('666')
+    //结束响应
+    response.end();
 
 }).listen(8000);
 console.log('server running at 127.0.0.1:8000')
---
+++ 01httpurl/02url.js
@@ -0,0 +1,23 @@
+var http = require('http');
+
+//引入url模块
+var url = require('url');
+
+
+http.createServer((request,response)=>{
+
+    response.writeHead(200,{'ContentType':'text/html;charset=utf-8'});
+
+    // 防止重复访问
+    if(request.url !== '/favicon.ico'){
+
+        // 第一个参数是地址，第二个参数是true表示把get传值转换为对象
+        var query = url.parse(request.url,true);
+        console.log(query);
+    }
+    
+    response.end();
+
+}).listen(8000);
+
+console.log('server running at 127.0.0.1:8000');
