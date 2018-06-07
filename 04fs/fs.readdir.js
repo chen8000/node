d009f14c0104f1458bc8ace3adfaf69e56d77cc2
zhanghui.chen <172360937@qq.com>
2018-06-07 20:11:09 +0800

    读取目录

--- 04fs/fs.readdir.js
+++ 04fs/fs.readdir.js
@@ -12,27 +12,18 @@
     fs.unlink 删除文件
 */
 
+
+// fs.readdir 读取目录
+
 //导入fs模块
 var fs = require('fs');
 
-
-//fs.appendFile  追加文件
-
-
-// 没有文件则创建，
-// 有文件则在文件里写内容，
-// 不会覆盖之前写入的内容，
-// 会在之前的内容后面追加内容
-fs.appendFile('appendFile.txt','zhanghui.chen 666',(err)=>{
+//读取目录
+fs.readdir('html',(err,res)=>{
     if(err){
         console.log(err);
         return;
     }
-
-    console.log('追加成功');
-});
-
-
-
-
+    console.log(res);
+})
 
--- 04fs/fs.writeFile.js
+++ 04fs/fs.writeFile.js
@@ -6,6 +6,7 @@
     fs.writeFile 创建写入文件
     fs.appendFile 追加文件
     fs.readFile 读取文件
+    fs.readdir 读取目录
     fs.rename 重命名
     fs.rmdir 删除目录
     fs.unlink 删除文件
