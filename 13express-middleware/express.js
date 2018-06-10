1230068282ece10fecfb1a101e7df678a5f231ee
chen8000 <13126618000@163.com>
2018-06-10 19:38:07 +0800

    中间件，匹配所有路由

--- 13express-middleware/express.js
+++ 13express-middleware/express.js
@@ -1,10 +1,33 @@
 
 
 
+// express 中间件
+
 const express = require('express');
 const app = new express();
 
 app.listen(8000,'127.0.0.1');
+
+/*
+中间件
+表示匹配任何路由
+
+应用级中间件
+
+next() 表示向下👇执行，继续向下匹配
+*/ 
+
+app.use((request, response, next) => {
+    
+    //打印一个时间
+    console.log(new Date());
+
+    next();
+})
+
+
+
+
 
 app.get('/', (request,response) => {
 
