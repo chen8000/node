703a7afe1b24ca1332af8217fc761a431745d47f
chen8000 <13126618000@163.com>
2018-06-10 21:38:02 +0800

    设置域名访问cookie

--- 14cookie-parser/cookie.js
+++ 14cookie-parser/cookie.js
@@ -28,4 +28,39 @@ app.get('/set', (request, response) => {
 
     response.send('设置cookie成功');
 
-})
+});
+
+//cookie的一些设置参数
+//都写到第三个参数的打括号里
+/*
+domain --设置域名访问cookie  
+{domain:'.aaa.com'}
+    -- 表示 www.aaa.com 可以访问cookie news.aaa.com 也可以访问cookie (多个二级域名共享这个cookie)
+{domain:'www.aaa.com'}
+    -- 表示 只有www.aaa.com 可以访问到这个cookie
+    
+maxAge  --设置过期时间
+secure  --值为 true 时 在 http 协议中是无效的，在 https 中才有效
+httpOnly --（服务器端可以设置cookie，客户端不可以设置cookie）微软对cookie做的拓展，如果设置为true时，通过 js applet等将无法获取到cookie,防止xss攻击产生。
+
+
+*/
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
