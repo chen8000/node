6176885ea8535e27af30393aaac4e6bf7767190f
chen8000 <13126618000@163.com>
2018-06-14 16:02:34 +0800

    学完了

--- project/modules/routes/admin/routes/user.js
+++ project/modules/routes/admin/routes/user.js
@@ -1,10 +1,28 @@
 
 const express = require('express');
 const router = express.Router();
+const db = require('../../../mongodb/db');
 
 router.get('/', (request, response) => {
 
-    response.send('user');
+    db.find('user', {}, (data) => {
+
+        response.render('admin/user/index', {list:data});
+    })
+});
+
+//增加用户
+router.get('/add', (request, response) => {
+
+    response.send('增加用户');
+
+    // response.render('/admin/user/add');
+});
+
+//删除用户
+router.get('/edit', (request, response) => {
+
+    response.send('修改用户');
 })
 
 
