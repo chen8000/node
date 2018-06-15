e6d66ca4ddfc1074238478dbe1e06f143fc16901
chen8000 <13126618000@163.com>
2018-06-15 15:18:32 +0800

    koa-ejs

---
+++ koa/koa-ejs/app.js
@@ -0,0 +1,49 @@
+
+
+const Koa = require('koa');
+const router = require('koa-router')();
+const views = require('koa-views');
+
+const app = new Koa();
+
+
+//配置第三方中间件
+// 以.html结尾的模版引擎
+// app.use(views('views', { map : { html : 'ejs'}}));
+// 从views里找模版， 使用ejs模版引擎  以 .ejs 结尾
+app.use(views('views', { extension:'ejs' }));
+
+router.get('/', async (ctx) => {
+
+    await ctx.render('index');
+})
+
+
+
+
+
+
+
+//启动路由
+app
+    .use(router.routes()) // 启动路由
+    .use(router.allowedMethods()); //  根据上下文环境配置 response 响应头
+
+
+
+app.listen(8000);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
