7d7fbd87020cc34f404c15cb22f2f179334a90c7
chen8000 <13126618000@163.com>
2018-06-15 19:08:47 +0800

    获取post 传值

--- koa/getpost/post.js
+++ koa/getpost/post.js
@@ -5,19 +5,38 @@ const Koa = require('koa');
 const router = require('koa-router')();
 const views = require('koa-views');
 
+//使用koa-bodyparser模块获取post提交数据
+const bodyParser = require('koa-bodyparser');
+
 const app = new Koa();
 
 //应用ejs模版
 app.use(views('views', { extension : 'ejs' }));
+app.use(bodyParser()); //配置中间件
+
+
+router.get('/', async (ctx) => {
+
+    await ctx.render('index');
+})
+
+//接收post提交过来的数据
+router.post('/doAdd', async (ctx) => {
+
+    //拿到post提交的数据
+    console.log(ctx.request.body)
+
+    ctx.body = ctx.request.body;
+    
+})
 
 
 
 
 
-// 启动路由
-app
-    .use(router.routes()) // 启动路由
-    .use(router.allowedMethods()); //  根据上下文环境配置 response 响应头
+
+app.use(router.routes());// 启动路由
+app.use(router.allowedMethods()); //  根据上下文环境配置 response 响应头
 
 
 app.listen(8000);
