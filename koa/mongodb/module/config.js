f75500b4b4eb45ef354d2a470e12e76ec4bd7d5b
chen8000 <13126618000@163.com>
2018-06-19 20:26:41 +0800

    收工

---
+++ koa/mongodb/module/config.js
@@ -0,0 +1,23 @@
+
+
+// 配置文件
+
+let app = {
+
+    dbUrl : 'mongodb://localhost:27017',
+    dbName : 'koa'
+
+}
+
+module.exports = app;
+
+
+
+
+
+
+
+
+
+
+
---
+++ koa/mongodb/module/db.js
@@ -0,0 +1,33 @@
+
+
+
+// db 库
+const MongoClient = require('mongodb').MongoClient;
+
+const config = require('./config');
+
+
+class Db {
+
+    // 构造函数
+    constructor () {
+        
+    }
+
+    // 连接数据库
+    connect () {
+        
+    }
+
+}
+
+
+
+
+
+
+
+
+
+
+
