696c0cbc9ef66a22718ef7e02ebe3604ec1b6d9c
chen8000 <13126618000@163.com>
2018-06-25 15:15:25 +0800

    可以继续了哈哈哈哈

--- koa/project/module/tools.js
+++ koa/project/module/tools.js
@@ -10,6 +10,8 @@ const toos = {
 
 
 
+
+
 module.exports = toos;
 
 
--- koa/project/routes/admin/login.js
+++ koa/project/routes/admin/login.js
@@ -10,22 +10,25 @@ const dbName = 'admin';
 
 router.get('/',async (ctx)=>{
     await ctx.render('admin/login');
-})
+});
 
 
 router.post('/doLogin',async (ctx)=>{
     
-
+    //拿到post传值
     let username = ctx.request.body.username;
-
     let password = ctx.request.body.password;
 
+    // 去数据库匹配数据
     let result = await DB.find(dbName, {"username":username, "password":tools.md5(password)});
 
+    // 有数据，证明登陆成功了，
     if(result.length>0){
 
+        //把数据存到session里
         ctx.session.userinfo = result[0];
 
+        //跳到admin页面
         ctx.redirect(ctx.state.__HOST__+'/admin');
     }else{
         console.log('失败');
