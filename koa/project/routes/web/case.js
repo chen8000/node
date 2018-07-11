997185e7f901eef4c33dbda306c27341e8073147
chen8000 <13126618000@163.com>
2018-07-11 15:44:12 +0800

    使用作符号

--- koa/project/routes/web/case.js
+++ koa/project/routes/web/case.js
@@ -11,36 +11,26 @@ const DB = require('../../module/db');
 
 // 成功案例case
 router.get('/', async (ctx) => {
+
+    let tab = ctx.query.pid;
+    
     let maxClassification = await DB.find("articlecate", [{"pid":"5b345495518729038178f082"}]);
     let inArr = [];
 
-    for(let i = 0; i<maxClassification.length; i++){
-        inArr.push(maxClassification[i]._id.toString());
+    if(!tab){
+        for(let i = 0; i<maxClassification.length; i++){
+            inArr.push(maxClassification[i]._id.toString());
+        }
+    }else{
+        inArr.push(tab);
     }
+   
 
+    // 使用$in操作符查询数据
     let minClassification = await DB.find('article', [{"pid":{$in:inArr}}]);
-    // let 
-
-    // 大分类是个集合 
-    // 小分类是个集合（把小分类库里的数据都显示出来）
-    // 点击大分类时带过来大分类的id  去小分类库里去查询 把对应的内容查出来
-
-
-    
-
-    // let minClassification = await DB.find('article', [{}]);
 
     await ctx.render('index/case', { maxClassification, minClassification});
 });
-
-// 服务行业  service
-
-// 教育行业  education
-
-// 房地产行业  property
-
-// 能源行业  energy
-
 
 
 
